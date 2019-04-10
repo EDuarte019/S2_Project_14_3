8e202ab9eaa5b29e759889111a96b8a8ba8b075c
Evelyn Duarte <eduarte019@west-mec.org>
2019-04-09 18:10:28 -0700

    Final Commit
    made if, else if statements and if else statements that will place the content within. ready for review.

--- js_tree.js
+++ js_tree.js
@@ -45,6 +45,7 @@ var wsCount = 0;
 //The makeTree function will load as the window does
 window.onload = makeTree;
 
+//For the makeTree function the aside element is created and to place the h1 within, you must append that variable to the variable where you created the aside element. Then, i simply declared the function makeBranches. Lastly, displayed all nodes by getting their id, giving propert of textcontent. 
 function makeTree() {
       var eD = document.createElement("aside");
       eD.setAttribute("id", "treeBox");
@@ -57,22 +58,52 @@ function makeTree() {
 
       eD.appendChild(nodeList);
 
-      var sourceArticle = document.querySelectorAll("#main article");
+      var sourceArticle = document.querySelector("#main article");
 
       makeBranches(sourceArticle, nodeList);
+
+      document.getElementById("totalNodes").textContent = nodeCount;
+      document.getElementById("elemNodes").textContent = elemCount;
+      document.getElementById("textNodes").textContent = textCount;
+      document.getElementById("wsNodes").textContent = wsCount;
 }
 
 function makeBranches(treeNode, nestedList) {
-      nodeCount += 1;
+      //increases by increments of 1, with creating an li element. then, within, there is text. then appending the spanElem within liElem and liElem within nestedelem.
+      nodeCount++;
       var liElem = document.createElement("li");
-      var spanElem = document.createElement("span");
       liElem.innerHTML += "+--";
+      var spanElem = document.createElement("span");
 
       liElem.appendChild(spanElem);
       nestedList.appendChild(liElem);
 
-      if (treeNode === null) {
-            elemCount += 1;
+      //Created an if else staments which claims that if treeNode is equal to 1 then, it will do he following. Otherwise, if it equals 3 then, it will do the following.
+      if (treeNode.nodeType === 1) {
+            elemCount++;
+            spanElem.setAttribute("class", "elementNode");
+            spanElem.textContent = "<" + treeNode.nodeName + ">"
+      } else if (treeNode.nodeType === 3) {
+            textCount++;
+            var textString = treeNode.nodeValue;
+            // within the else if, is an if, else statement which states if the condition is the function then it will have an increase of 1, attribute and textContent. otherwise, it will have a different attribute and textContent.
+            if (isWhiteSpaceNode(textString)) {
+                  wsCount++;
+                  spanElem.setAttribute("class", "whiteSpaceNode");
+                  spanElem.textContent = "#text";
+            } else {
+                  spanElem.setAttribute("class", "textNode");
+                  spanElem.textContent = textString;
+            }
+            // Within, the if statement, there is a for loop that will loop through the treeNode.
+      }
+      if (treeNode.childNodes.length > 0) {
+            var newList = document.createElement("ol");
+            newList.innerHTML = "|";
+            nestedList.appendChild(newList);
+            for (var n = treeNode.firstChild; n !== null; n = n.nextSibling) {
+                  makeBranches(n, newList);
+            }
       }
 }
 
